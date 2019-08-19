09571484b20502974df57413672d9a534e88f1e2
Chen Wang <wangcees@gmail.com>
2019-08-19 18:40:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,6 +123,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  arr = programmer_hash[:dennis_ritchie][:languages]
+  arr << "Assembly"
   
 end
