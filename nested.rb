3cafab2ef155307e8ad90e328600b2a29b339cc7
Chen Wang <wangcees@gmail.com>
2019-08-19 18:01:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  hash = programmer_hash[:yukihiro_matsumoto] = {
+  programmer_hash[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }
+  
+  hash = programmer_hash[:yukihiro_matsumoto]
     
 end
 
